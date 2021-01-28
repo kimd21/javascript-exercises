f50cd4ad0189177f62a2ab2e3e1280046ddb4a37
Dong Kyu Kim <dongkyukim00@gmail.com>
2021-01-28 11:54:14 -0600

    updated sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+  let largeNumber = 0;
+  let smallNumber = 0;
+  if (!Number.isInteger(args[0]) || !Number.isInteger(args[1])) {return 'ERROR';}
+  if (args[0] < 0 || args[1] < 0) {return 'ERROR';}
+  if (args[0] > args[1]) {
+    largeNumber = args[0];
+    smallNumber = args[1];
+  }
+  else {
+    largeNumber = args[1];
+    smallNumber = args[0];
+  }
+  let sum = 0;
+  for (let i = smallNumber; i <= largeNumber; i++) {
+    sum += i;
+  }
+  return sum;
 }
-
 module.exports = sumAll
