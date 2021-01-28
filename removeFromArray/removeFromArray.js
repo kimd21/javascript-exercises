39c1b663a3d1ce791921a8831edbb25118c54e6b
Dong Kyu Kim <dongkyukim00@gmail.com>
2021-01-28 11:31:32 -0600

    updated removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+  // for (let i = 0; i < arr1.length; i++) {
+  //   for (let j = 0; j < arr2.length; j++) {
+  //     if (arr1[i] === arr2[j]) {
+  //       arr1.splice(i, 1);
+  //       i--;
+  //     }
+  //   }
+  // }
+  // return arr1;
+  const arr = args[0];
+  return arr.filter(val => !args.includes(val));
 }
 
 module.exports = removeFromArray
