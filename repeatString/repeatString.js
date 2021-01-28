fa794b9207dd0dd7e186f8c1d8697b0cbdcbfe6f
Dong Kyu Kim <dongkyukim00@gmail.com>
2021-01-28 10:27:52 -0600

    updated repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberOfTimes) {
+let str = '';
+if (numberOfTimes < 0) {return 'ERROR';}
+for (let i = 0; i < numberOfTimes; i++) {
+  str += word;
+}
+return str;
 }
 
 module.exports = repeatString
