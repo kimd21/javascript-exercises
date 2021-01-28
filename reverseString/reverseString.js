b26c6dd7770f81620561b19d5e05283def152da3
Dong Kyu Kim <dongkyukim00@gmail.com>
2021-01-28 10:33:57 -0600

    updated reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let str = '';
+  for (let i = 0; i < word.length; i++) {
+    str += word[word.length - i - 1];
+  }
+  return str;
 }
 
 module.exports = reverseString
