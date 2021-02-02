852f280cc6d37f09c2a5800a813ae37964d22015
Dong Kyu Kim <dongkyukim00@gmail.com>
2021-02-01 20:11:34 -0600

    updated fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(idx) {
+  if (idx < 0) return 'OOPS';
+  if (idx == 0) return 0;
+  let n = parseInt(idx);
+  let ctr = 1;
+  let n1 = 0;
+  let n2 = 1;
+  while (ctr <= n) {
+    tmp = n1 + n2;
+    n2 = n1;
+    n1 = tmp;
+    ctr++;
+  }
+  return n1;
 }
 
 module.exports = fibonacci
