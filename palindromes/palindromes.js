961341842ccbf582f0e9e6422e25d35f798bc180
Dong Kyu Kim <dongkyukim00@gmail.com>
2021-02-01 19:25:58 -0600

    updated palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function() {
+const palindromes = function(str) {
+  let modStr = str
+    .toLowerCase()
+    .trim()
+    .replace(/\W|\d|\s/g, '');
+  
+  return modStr === modStr.split('').reverse().join('');
 
+  // const len = modStr.length;
+  // let rStr = '';
+  // let lStr = '';
+  // if (len % 2 === 0) {
+  //   rStr = modStr.slice(0, len/2);
+  //   lStr = modStr.slice(len/2 + 1, len)
+  // }
+  // else {
+  //   rStr = modStr.slice(0, Math.floor(len/2));
+  //   lStr = modStr.slice(Math.floor(len/2) + 1, len);
+  // }
+  // return rStr === lStr.split('').reverse().join('')
 }
 
 module.exports = palindromes
